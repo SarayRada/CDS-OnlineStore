fecdd524533b6658eb50c848110904a1e814d4bb
Saray Rada <SarayRadaReboso@outlook.es>
2022-09-01 20:57:26 +0200

    de word a codigo

--- OnlineStore/src/main/kotlin/OnlineSTORE.kt
+++ OnlineStore/src/main/kotlin/OnlineSTORE.kt
@@ -1,2 +1,43 @@
 class OnlineSTORE {
 }
+
+class Products{
+    private var productsList: MutableList<Product> = mutableListOf()
+}
+
+class Product(name:String,image:String,reference:Int,id: Int, price: Double,description: String ){
+    fun actualiceData(){
+    }
+    fun showData(){}
+}
+
+class Carrito{
+    private var productsInTheShoppingBasket: MutableList<Product> = mutableListOf()
+    fun addProductsToTheBasket(){
+    }
+    fun deleteProducts(){}
+    fun showBaskets{
+        // for each
+    }
+}
+/*
+Crear una clase {
+    Var lista de productos{}
+    Fun add product{}
+    fun deleteProduct (){
+    }
+}*/
+
+class Costumer {
+    private var myShoppintList: Carrito = Carrito()
+    fun pay(){}
+    fun askForTheCheck(){}
+    fun seeMyBasket(){}
+}
+
+class Receipt {
+
+}
+
+Lógica de dominio
+Capa de presenta
