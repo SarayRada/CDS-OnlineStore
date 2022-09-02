c49c2b39a5d7adb2f1c06aa57e6acb9f4a091740
Saray Rada <SarayRadaReboso@outlook.es>
2022-09-02 12:00:25 +0200

    first steps

--- OnlineStore/src/main/kotlin/OnlineSTORE.kt
+++ OnlineStore/src/main/kotlin/OnlineSTORE.kt
@@ -1,43 +1,35 @@
-class OnlineSTORE {
+class OnlineStore{
+    private var productsCatalogue: Catalogue = Catalogue()
+    private var allProduct: Products =Products()
 }
 
-class Products{
-    private var productsList: MutableList<Product> = mutableListOf()
+class Screen{
+    fun showProducts(){}
 }
 
-class Product(name:String,image:String,reference:Int,id: Int, price: Double,description: String ){
-    fun actualiceData(){
-    }
-    fun showData(){}
+class Catalogue(){
+    var productInformation 
+
 }
 
-class Carrito{
-    private var productsInTheShoppingBasket: MutableList<Product> = mutableListOf()
-    fun addProductsToTheBasket(){
-    }
-    fun deleteProducts(){}
-    fun showBaskets{
-        // for each
+class Products(){
+    var listOfProducts: MutableList<Product> = mutableListOf()
+    fun showProductsInformation (){
+        return
     }
 }
-/*
-Crear una clase {
-    Var lista de productos{}
-    Fun add product{}
-    fun deleteProduct (){
-    }
-}*/
-
-class Costumer {
-    private var myShoppintList: Carrito = Carrito()
-    fun pay(){}
-    fun askForTheCheck(){}
-    fun seeMyBasket(){}
+class Client{
+    fun seeScreen(){}
+    fun chooseProducts(){}
 }
 
-class Receipt {
-
+class Product(){
+    private var productImage:Char = ' '
+    private var productDescription:String = ""
+    // <SHORT_DESCRIPTION>, <HIGHLIGHTED_ATTRIBUTE>.
+    private var productPrice: Double= 0.0
+    // Price: <Price> €
+    private var productReference:String = ""
+    // Reference: <REFERENCE>
+    // linea en blanco al final de cada producto
 }
-
-Lógica de dominio
-Capa de presenta
