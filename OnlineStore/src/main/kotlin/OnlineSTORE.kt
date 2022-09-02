366d00667aad3b5ee1087ba7691c79df40794a7a
Saray Rada <SarayRadaReboso@outlook.es>
2022-09-02 16:05:04 +0200

    working in process

--- OnlineStore/src/main/kotlin/OnlineSTORE.kt
+++ OnlineStore/src/main/kotlin/OnlineSTORE.kt
@@ -1,35 +1,79 @@
 class OnlineStore{
     private var productsCatalogue: Catalogue = Catalogue()
-    private var allProduct: Products =Products()
+    fun addProductToList(product: Product){
+        Products().addToMyList(product)
+    }
+class Products(){
+    private var listOfProducts: MutableList<Product> = mutableListOf()
+    fun addToMyList(product: Product){
+        listOfProducts.add(product)
+    }
+    fun showMyList():MutableList<Product>{
+        return listOfProducts
+    }
+}
+class Catalogue(){
+    private var myProductInformation: MutableList<String> = mutableListOf()
+    fun takeProductsInformation() {
+        for (products in Products().showMyList()) {
+                myProductInformation.add(products.showInformation().joinToString {"/n"})
+            }
+        }
+    fun showCatalogue():MutableList<String>{
+        return myProductInformation
+    }
 }
 
 class Screen{
-    fun showProducts(){}
-}
+    private var myCatalogue: MutableList<String> = mutableListOf()
+    init {
+        fun seeProducts(){
+            for (information in Catalogue().showCatalogue()){
+                myCatalogue.add("$information\n\n")
+            }
+        }
+    }
 
-class Catalogue(){
-    var productInformation 
-
-}
-
-class Products(){
-    var listOfProducts: MutableList<Product> = mutableListOf()
-    fun showProductsInformation (){
-        return
+    fun seeCatalogue(): MutableList<String>{
+        return myCatalogue
     }
 }
+
+
+
 class Client{
-    fun seeScreen(){}
+    fun seeCatalogue():MutableList<String>{
+        return Screen().seeCatalogue()
+    }
     fun chooseProducts(){}
 }
 
-class Product(){
-    private var productImage:Char = ' '
+
+class Product(image:String= "", description: String="", price: String= "", reference:String= ""){
+    private var productImage:String = " "
     private var productDescription:String = ""
-    // <SHORT_DESCRIPTION>, <HIGHLIGHTED_ATTRIBUTE>.
-    private var productPrice: Double= 0.0
-    // Price: <Price> €
+    private var productPrice: String= ""
     private var productReference:String = ""
-    // Reference: <REFERENCE>
-    // linea en blanco al final de cada producto
+    init {
+        productImage = image
+        productDescription = description
+        productPrice = price
+        productReference =reference
+    }
+    fun showInformation(): List <String>{
+        return listOf <String>(productImage, productDescription, "Reference: $productReference","\uD83D\uDCB0Price: $productPrice")
+    }
+}
+
+fun main(){
+    var televisionTDSystem: Product = Product("\uD83D\uDCFA", "Glamorous panoramic television, 13 inches\n" +
+            "With this panoramic television, your friday nights will be boring no more.", "300.99 €","W2C")
+    var piano: Product = Product("\uD83C\uDFB9", "Untuned musical keyboard, 4 octaves\n" +
+            "Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you."
+            "1003.00 €","X4A")
+    var myOnlineStore:OnlineStore = OnlineStore()
+    myOnlineStore.addProductToList(televisionTDSystem)
+    myOnlineStore.addProductToList(piano)
+    Catalogue().showCatalogue()
+println(Client().seeCatalogue())
 }
