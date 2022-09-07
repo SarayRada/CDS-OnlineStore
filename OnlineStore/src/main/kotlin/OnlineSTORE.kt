d6a489eae8304ed1cb55e492d0f066f88fc6921a
Saray Rada <SarayRadaReboso@outlook.es>
2022-09-07 13:47:16 +0200

    first result

--- OnlineStore/src/main/kotlin/OnlineSTORE.kt
+++ OnlineStore/src/main/kotlin/OnlineSTORE.kt
@@ -1,17 +1,28 @@
 class OnlineStore {
-    private var store: Store = Store()
-    fun showProducts(){
-        for (product in store.showMyProducts()){
-        }
+    private var stock: Store = Store()
+    private var output: Screen = Screen(stock)
+    fun showStock(){
+        output.showInformation()
     }
 
 }
 
-
+class Screen(store:Store){
+    private var storeToShow: Store = store
+    fun showInformation(){
+        for (products in storeToShow.showMyProducts()){
+            println(products.showProductImage())
+            println(products.showProductDescription())
+            println(products.showProductAttribute())
+            println("\uD83D\uDCB0 Price: ${products.showProductPrice()}€")
+            println("Reference: ${products.showProductReference()}\n")
+        }
+    }
+}
 class Store (){
-    private var allMyProducts: MutableList<Product> = mutableListOf(Product("\uD83D\uDCFA","Glamorous panoramic television, 13 inches", "With this panoramic television, your friday nights will be boring no more.", 300.99, "W2C"), Product("\uD83C\uDFB9", "Untuned musical keyboard, 4 octaves", "Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home and your neighbour will be ready to move to a building far away from you. ", 1003.00, "X4A"))
+    private var productsStock:  MutableList<Product> = mutableListOf(Product("\uD83D\uDCFA","Glamorous panoramic television, 13 inches", "With this panoramic television, your friday nights will be boring no more.", 300.99, "W2C"), Product("\uD83C\uDFB9", "Untuned musical keyboard, 4 octaves", "Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home\nand your neighbour will be ready to move to a building far away from you. ", 1003.00, "X4A"))
     fun showMyProducts():MutableList<Product>{
-        return allMyProducts
+        return productsStock
     }
 
 
@@ -48,5 +59,5 @@ class Product(image:String, description: String, attribute: String, price: Doubl
 
 fun main(){
     var onlineStore: OnlineStore = OnlineStore()
-    println(onlineStore.showProducts())
+    onlineStore.showStock()
 }
