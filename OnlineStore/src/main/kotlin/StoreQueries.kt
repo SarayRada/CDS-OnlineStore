68b27b25d58463a46e46658d35379affacb7e9aa
Saray Rada <SarayRadaReboso@outlook.es>
2022-10-30 13:23:35 +0100

    Añadiendo el stock en stockQueries por 4º regla del diseño simple

--- OnlineStore/src/main/kotlin/StoreQueries.kt
+++ OnlineStore/src/main/kotlin/StoreQueries.kt
@@ -1,5 +1,23 @@
-class StoreQueries (private var productStore:ProductStore){
-    private val stock get() = productStore.stock
+class StoreQueries (){
+    private val stock:  MutableList<Product> = mutableListOf(
+        Product("\uD83D\uDCFA",
+            "Glamorous panoramic television, 13 inches",
+            "With this panoramic television, your friday nights will be boring no more.",
+            "The screen is composed of 14 million pixels.\nThanks to its low energy consumption design, your bills will stay low.",
+            300.99,
+            "W2C"),
+        Product("\uD83C\uDFB9",
+            "Untuned musical keyboard, 4 octaves",
+            "Tired of your noisy neighbourgh? Play this untuned musical keyboard for two hours at home\nand your neighbour will be ready to move to a building far away from you.",
+            "",
+            1003.00,
+            "X4A"),
+        Product("\uD83D\uDCFA",
+            "Glamorous panoramic television, 13 inches",
+            "With this panoramic television, your friday nights will be boring no more.",
+            "The screen is composed of 14 million pixels.\nThanks to its low energy consumption design, your bills will stay low.",
+            300.99,
+            "W2C"))
     fun getAProduct(reference: String): Product?{
         return try {
            stock.first { it.reference == reference }
--- OnlineStore/src/main/kotlin/main.kt
+++ OnlineStore/src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val storeQueries: StoreQueries =StoreQueries (ProductStore())
+    val storeQueries: StoreQueries =StoreQueries()
     val screen: Screen = Screen()
     val shoppingCart: ShoppingCart = ShoppingCart()
     val onlineStore: OnlineStore = OnlineStore(storeQueries, screen, shoppingCart)
